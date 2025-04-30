f31ffef4619f5bd1b7ada20cd35dddb9e82c79fa
hachikyuji <muringjohncarlo@gmail.com>
2025-04-30 09:00:37 +0800

    crsf origins

--- sociallearning/sociallearning/settings.py
+++ sociallearning/sociallearning/settings.py
@@ -169,3 +169,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://forumly.onrender.com/',
+]
